1393523330f4b02a52da9c20b1160c57f992e2ee
Josmas Flores <josmasflores@gmail.com>
2011-02-05 11:56:42 +0000

    Changed mouse event to context menu and grabbing username and server data from session

--- src/classes/org/jdesktop/wonderland/modules/restsamplemodule/client/RESTSampleCell.java
+++ src/classes/org/jdesktop/wonderland/modules/restsamplemodule/client/RESTSampleCell.java
@@ -17,34 +17,35 @@
  */
 package org.jdesktop.wonderland.modules.restsamplemodule.client;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jdesktop.wonderland.common.cell.state.CellClientState;
 import org.jdesktop.wonderland.client.cell.Cell;
 import org.jdesktop.wonderland.client.cell.Cell.RendererType;
 import org.jdesktop.wonderland.client.cell.CellCache;
 import org.jdesktop.wonderland.client.cell.CellRenderer;
-import org.jdesktop.wonderland.client.cell.ChannelComponent;
 import org.jdesktop.wonderland.client.cell.annotation.UsesCellComponent;
+import org.jdesktop.wonderland.client.contextmenu.ContextMenuActionListener;
 import org.jdesktop.wonderland.client.contextmenu.ContextMenuItem;
+import org.jdesktop.wonderland.client.contextmenu.ContextMenuItemEvent;
 import org.jdesktop.wonderland.client.contextmenu.SimpleContextMenuItem;
 import org.jdesktop.wonderland.client.contextmenu.cell.ContextMenuComponent;
 import org.jdesktop.wonderland.client.contextmenu.spi.ContextMenuFactorySPI;
-import org.jdesktop.wonderland.client.input.Event;
-import org.jdesktop.wonderland.client.input.EventClassListener;
-import org.jdesktop.wonderland.client.jme.input.MouseButtonEvent3D;
-import org.jdesktop.wonderland.client.jme.input.MouseEvent3D.ButtonId;
+import org.jdesktop.wonderland.client.login.LoginManager;
 import org.jdesktop.wonderland.client.scenemanager.event.ContextEvent;
 import org.jdesktop.wonderland.common.cell.CellID;
 import org.jdesktop.wonderland.common.cell.CellStatus;
 import org.jdesktop.wonderland.modules.restsamplemodule.common.RESTSampleCellClientState;
+import org.jdesktop.wonderland.modules.restsamplemodule.client.rest.RestGateway;
 
 public class RESTSampleCell extends Cell {
     
     private String shapeType = null;
     private String textureURI = null;
     private RESTSampleCellRenderer renderer = null;
-    private MouseEventListener listener = null;
 
-    @UsesCellComponent private ContextMenuComponent contextComp = null;
+    @UsesCellComponent
+    private ContextMenuComponent contextComp = null;
     private ContextMenuFactorySPI menuFactory = null;
 
     public RESTSampleCell(CellID cellID, CellCache cellCache) {
@@ -82,10 +83,6 @@ public class RESTSampleCell extends Cell {
         super.setStatus(status, increasing);
 
         if (status == CellStatus.INACTIVE && increasing == false) {
-            if (listener != null) {
-                listener.removeFromEntity(renderer.getEntity());
-                listener = null;
-            }
 
             if (menuFactory != null) {
                 contextComp.removeContextMenuFactory(menuFactory);
@@ -93,27 +90,49 @@ public class RESTSampleCell extends Cell {
             }
         }
         else if (status == CellStatus.RENDERING && increasing == true) {
-            if (listener == null) {
-                listener = new MouseEventListener();
-                listener.addToEntity(renderer.getEntity());
+            if (menuFactory == null) {
+                final MenuItemListener l = new MenuItemListener();
+                menuFactory = new ContextMenuFactorySPI() {
+                    public ContextMenuItem[] getContextMenuItems(ContextEvent event) {
+                        return new ContextMenuItem[]{
+                                    new SimpleContextMenuItem("Register me", l)
+                                };
+                    }
+                };
+                contextComp.addContextMenuFactory(menuFactory);
             }
         }
     }
 
-    class MouseEventListener extends EventClassListener {
-        @Override
-        public Class[] eventClassesToConsume() {
-            return new Class[]{MouseButtonEvent3D.class};
-        }
-
-        @Override
-        public void commitEvent(Event event) {
-            MouseButtonEvent3D mbe = (MouseButtonEvent3D) event;
-            if (mbe.isClicked() == false || mbe.getButton() != ButtonId.BUTTON1) {
-                return;
-            }
+    class MenuItemListener implements ContextMenuActionListener {
+        public void actionPerformed(ContextMenuItemEvent event) {
+            //TODO this instead of changing shape, will have to call the post action
             shapeType = (shapeType.equals("BOX") == true) ? "SPHERE" : "BOX";
             renderer.updateShape();
+            String URLToPostTO = createURLToPostTo();
+            String userString = createMessageToPost();
+            try {
+                RestGateway.restPOSTConnection(URLToPostTO, userString.toString());
+            } catch (Exception ex) { //TODO if an exception happens, have to tell the user!
+                //TODO could be great to have a Call to EZScript create HUD with a message (or can it be done
+                //with huddialog?)
+                Logger.getLogger(RESTSampleCell.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+        private String createMessageToPost() {
+            StringBuffer userString = new StringBuffer("");
+            userString.append("<user>");
+            userString.append("<first-name>" + LoginManager.getPrimary().getUsername() + "</first-name>");
+            userString.append("<last-name>" + LoginManager.getPrimary().getServerNameAndPort() + "</last-name>");
+            userString.append("</user>");
+            return userString.toString();
+        }
+
+        private String createURLToPostTo() {
+            //TODO the first part of this should be read from a properties file
+            return "http://173.45.228.42:3000/users.xml";
         }
     }
 }
--- src/classes/org/jdesktop/wonderland/modules/restsamplemodule/client/rest/RestGateway.java
+++ src/classes/org/jdesktop/wonderland/modules/restsamplemodule/client/rest/RestGateway.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  *
  * @author jos
  */
-class RestGateway {
+public class RestGateway {
 
     private static final Logger LOGGER =
             Logger.getLogger(RestGateway.class.getName());
